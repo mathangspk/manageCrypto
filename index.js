ff90ae77d52b9aae21fdb691a5f843f57478a17f
mathangspk <mathangspk@gmail.com>
2023-02-05 22:19:36 +0700

    dfdf

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ async function getPrice(symbol) {
     try {
         const data = await client.getQuotes({ symbol: symbol })
         // console.log(data.data.symbol.quote.USD.price)
-        const name = data.data[Object.keys(data.data)[0]].name
+        //const name = data.data[Object.keys(data.data)[0]].name
         const price = data.data[Object.keys(data.data)[0]].quote.USD.price
+        //const quote = data.data[Object.keys(data.data)[0]].quote
         return price
-        console.log(price)
+        //console.log(quote)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+async function getMarketCapDominance(symbol) {
+    try {
+        const data = await client.getQuotes({ symbol: symbol })
+        // console.log(data.data.symbol.quote.USD.price)
+        //const name = data.data[Object.keys(data.data)[0]].name
+        //const price = data.data[Object.keys(data.data)[0]].quote.USD.price
+        const market_cap_dominance = data.data[Object.keys(data.data)[0]].quote.USD.market_cap_dominance
+        return market_cap_dominance
+        console.log(market_cap_dominance)
     } catch (e) {
         console.log(e)
     }
@@ -46,7 +61,7 @@ async function getDataFromGoogleSheet() {
     return (getRows.data.values)
 };
 
-async function updateDataGoogleSheet(symbol, value, row) {
+async function updatePriceGoogleSheet(symbol, value, row) {
     const auth = new google.auth.GoogleAuth({
         keyFile,
         scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -62,6 +77,32 @@ async function updateDataGoogleSheet(symbol, value, row) {
     const spreadsheetId = '1W8tJBVXTPKBxpthhiLCvQwi8DNgFIq3kHj7BWJfDQ-Y';
     //get metadata about spreadsheet 
     let range = String(`${symbol}!I` + row);
+    await googleSheets.spreadsheets.values.update({
+        auth,
+        spreadsheetId,
+        range,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [[value]]
+        }
+    })
+};
+async function updatePercentDominanceGoogleSheet(symbol, value, row) {
+    const auth = new google.auth.GoogleAuth({
+        keyFile,
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    })
+
+    //create client instance for auth
+
+    const client = await auth.getClient();
+    //instance of google sheets API
+
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const spreadsheetId = '1W8tJBVXTPKBxpthhiLCvQwi8DNgFIq3kHj7BWJfDQ-Y';
+    //get metadata about spreadsheet 
+    let range = String(`${symbol}!D` + row);
     await googleSheets.spreadsheets.values.update({
         auth,
         spreadsheetId,
@@ -103,8 +144,12 @@ async function updateTimeGoogleSheet(row) {
 async function updatePrice(symbol) {
     try {
         const priceBTC = await getPrice(symbol)
+        const percentDominance = await getMarketCapDominance(symbol)
         console.log(priceBTC)
-        await updateDataGoogleSheet(symbol, priceBTC, 3);
+        console.log(percentDominance)
+        await updatePriceGoogleSheet(symbol, priceBTC, 3);
+        await updatePercentDominanceGoogleSheet(symbol, percentDominance, 1);
+
     } catch (e) {
         console.log(e)
     }
